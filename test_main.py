16d4bdbe7061cc49683ed4bd49308c574a45c7ca
Kutay Sapanci <97249@wroclaw.merito.pl>
2026-05-28 22:14:00 +0200

    Force strict failure with ValueError

--- test_main.py
+++ test_main.py
@@ -1,4 +1,3 @@
-from main import calculate_sum
-
 def test_calculate_sum():
-    assert calculate_sum(2, 3) == 6
+    # Bu satır kodun direkt olarak hata verip durmasını sağlar
+    raise ValueError("Simulated Bug For Homework")
