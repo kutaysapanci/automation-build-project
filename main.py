6d6bd6a744c4aa02c522448a01282962a86f8903
kutaysapanci <kutaysapanc@gmail.com>
2026-05-29 11:20:55 +0200

    Version 1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-def calculate_sum(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Merhaba Flask!"
+
+@app.route("/about")
+def about():
+    return "Hakkımızda sayfası"
+
+if __name__ == "__main__":
+    app.run(debug=True)
